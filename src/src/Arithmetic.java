94d46801396f21b1f201d285bbff1efcdb6dde44
Jean Frg <jeanfrg@gmail.com>
2012-10-12 08:27:39 +0200

    eclipse project 1

---
+++ src/src/Arithmetic.java
@@ -0,0 +1,51 @@
+package src;
+
+public class Arithmetic {
+	
+	public double Add(double addThese[]) {
+		double add = 0;
+		for(int i = 0; i < addThese.length; i++) {
+			add += addThese[i];
+		}
+		
+		return add;
+	}
+	
+	public double Subtract(double subThese[]){
+		double sub = 0;
+		for(int i = 0; i < subThese.length; i++) {
+			sub -= subThese[i];
+		}
+		
+		return sub;
+	}
+	
+	public double Multiply(double multThese[]){
+		double mult = multThese[0];
+		for(int i = 1; i < multThese.length; i++) {
+			mult *= multThese[i];
+		}
+		
+		return mult;
+	}
+	
+	public double Divide(double divThese[]){
+		double div = divThese[0];
+		for(int i = 1; i < divThese.length; i++) {
+			div /= divThese[i];
+		}
+		
+		return div;
+	}
+
+	public double Average(double avrgThese[]) {
+		double avrg = 0;
+		for(int i = 0; i < avrgThese.length-1; i++) {
+			avrg += avrgThese[i];
+		}
+		
+		return avrg/avrgThese.length;
+	}
+	
+	
+}
---
+++ src/src/Main.java
@@ -0,0 +1,12 @@
+package src;
+
+public class Main {
+	public static void main(String args[]){
+		Arithmetic math = new Arithmetic();
+		
+		double arr[] = {200, 2};
+		System.out.println(math.Average(arr));
+		System.out.println(math.Multiply(arr));
+	}
+
+}
